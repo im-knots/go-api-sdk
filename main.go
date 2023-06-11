cdf74beae24de6c355cc38f5477ef84c121d5d84
im-knots <mrgarcia94@gmail.com>
2023-06-10 22:17:29 -0400

    implement multiple example service endpoints

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/im-knots/go-api-sdk/config"
@@ -11,12 +12,34 @@ import (
 type ExampleService struct{}
 
 func (m *ExampleService) RegisterRoutes(r *gin.Engine) {
-	r.GET("/example", m.ExampleHandler)
+	r.GET("/api/v1/", m.RootHandler)
+	r.GET("/api/v1/report", m.ReportHandler)
+	r.GET("/api/v1/crud", m.CrudHandler)
+	r.POST("/api/v1/input", m.InputHandler)
 }
 
-func (m *ExampleService) ExampleHandler(c *gin.Context) {
+func (m *ExampleService) RootHandler(c *gin.Context) {
 	log.Println("this is a test")
-	c.JSON(200, gin.H{"message": "Hello World!"})
+	c.JSON(http.StatusOK, gin.H{"message": "Hello World!"})
+}
+
+func (m *ExampleService) ReportHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "OK"})
+}
+
+func (m *ExampleService) CrudHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "OK"})
+}
+
+func (m *ExampleService) InputHandler(c *gin.Context) {
+	var jsonInput map[string]interface{}
+
+	if err := c.ShouldBindJSON(&jsonInput); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, jsonInput)
 }
 
 type MyConfig struct {
